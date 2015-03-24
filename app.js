3607c779d1e7bce4f79b5c980b07f8e4fe4047f3
Joe <joe.ekiert@gmail.com>
2015-03-24 23:18:29 +0000

    creating local mongoose db connection

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dbConfig = require('./config/db');
+var mongoose = require('mongoose');
+mongoose.connect(dbConfig.url);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');

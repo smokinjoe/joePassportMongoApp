7f415adfc57df2090d3946869d2526eaf4ec6225
Joe <joe.ekiert@gmail.com>
2015-03-24 23:16:30 +0000

    creating db.js node module from nitrous.io tutorial, hopefully the url property is correct

---
+++ config/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // utilizing local mongodb: http://help.nitrous.io/mongodb/
+  // if there are issues with connecting, scroll to the bottom
+  // and follow the directions on how to build your MongoClient
+  'url' : 'mongodb://127.0.0.1:27017/'
+  
+  // tutsplus tutorial mentions to utilize:
+  //'url' : 'mongodb://localhost/passport'
+};

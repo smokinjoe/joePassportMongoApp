e508dc8585a896a23b7c6a2c2b24e95c8aa0be89
Joe <joe.ekiert@gmail.com>
2015-03-25 18:34:35 +0000

    beginning to build out the login strategy a la passport

---
+++ passport/login.js
@@ -0,0 +1,27 @@
+passport.use('login', new LocalStrategy({
+    passReqToCallback: true
+  },
+  function (req, username, password, done) {
+    // check in mongo if a user with username exists or not
+    User.findOne({ 'username' : username },
+      function (err, user) {
+        // In case of any error, return using the done method
+        if (err)
+          return done(err);
+        // username does not exist, log error & redirect back
+        if (!user) {
+          console.log('User not found with username ', username);
+          return done(null, false, req.flash('message', 'User not found'));
+        }
+        // User exists but wrong password, log the error
+        if (!isValidPassword(user, password)) {
+          console.log('Invalid password');
+          return done(null, false, req.flash('message', 'Invalid password'));
+        }
+        // User and password both match, return user from
+        // done method which will be treated like success
+        return done(null, user);
+      }
+    );
+  }
+));

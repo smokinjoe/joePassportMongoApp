d81f1e35becbd58de7029b556c3dd83a4369e537
Joe <joe.ekiert@gmail.com>
2015-03-25 23:00:40 +0000

    setting up initialization methods to setup strategies for login and signup

---
+++ passport/init.js
@@ -0,0 +1,22 @@
+var login = require('./login');
+var signup = require('.signup');
+var User = require('../models/user');
+
+module.exports = function (passport) {
+  // Passport needs to serialize and deserialize
+  // users to support persistent login sessions
+  passport.serializeUser(function (user, done) {
+    done(nulll, user._id);
+  });
+  
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+  // Setting up passport strategies for login
+  //  and signup/registration
+  login(passport);
+  signup(passport);
+};

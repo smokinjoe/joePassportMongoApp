7d54c5498cf62057a06c32b0e601822a7e71652d
Joe <joe.ekiert@gmail.com>
2015-03-25 21:46:55 -0700

    updating db connection url

--- config/db.js
+++ config/db.js
@@ -2,8 +2,12 @@ module.exports = {
   // utilizing local mongodb: http://help.nitrous.io/mongodb/
   // if there are issues with connecting, scroll to the bottom
   // and follow the directions on how to build your MongoClient
-  'url' : 'mongodb://127.0.0.1:27017/'
-  
+  //'url' : 'mongodb://127.0.0.1:27017/'
+
   // tutsplus tutorial mentions to utilize:
   //'url' : 'mongodb://localhost/passport'
+
+  // I *think* this http://stackoverflow.com/a/3597922/355627 helps explain what's going on
+  // not sure how /passport will affect things
+  'url' : 'mongodb://127.0.0.1:27017/passport'
 };

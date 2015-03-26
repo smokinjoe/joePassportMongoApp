e069eb9e91b09f568caf7f244111822e046e3132
Joe <joe.ekiert@gmail.com>
2015-03-26 20:42:28 +0000

    order matters

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ var routes = require('./routes/index');
 
 var app = express();
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 // configuring Passport
 var passport = require('passport');
 var expressSession = require('express-session');
@@ -36,18 +48,6 @@ initPassport(passport);
 // Initialize Passport routes
 var routes = require('./routes/index')(passport);
 app.use('/', routes);
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
